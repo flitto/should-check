0b5144acaf19bdd9ccbd1a2656cf913b4cf6f294
Dan Kang <kang@flitto.com>
2014-09-01 22:28:34 +0900

    support multiple objects

--- index.js
+++ index.js
@@ -1,62 +1,67 @@
 var should = require('should');
 
-var gObjs;
+function init(gObj) {
 
-exports.init = function(objs) {
-  gObjs = objs;
-};
-
-var check = exports.check = function(name, obj) {
-  if (/[a-z]/.test(name[0])) {
-    obj.should.be.type(name);
-    return;
-  }
-
-  if (!gObjs)
-    throw new Error('Please call init() with obj definitions before call check()!');
-
-  if (!gObjs.hasOwnProperty(name))
-    throw new Error(name + ' is not defined!');
-
-  gObjs[name].forEach(function(prop){
-    var arr = prop.split(':')
-      , key = arr[0].trim()
-      , val
-      , type;
-
-    if (key.length == 0) {
-      obj.should.be.instanceOf(Array);
-
-      type = arr[1].trim();
-      type = type.replace('[', '').replace(']', '').trim();
-      obj.forEach(function(o){
-        check(type, o);
-      });
+  function check(name, obj) {
+    if (/[a-z]/.test(name[0])) {
+      obj.should.be.type(name);
       return;
     }
 
-    obj.should.have.property(key);
-    val = obj[key];
+    if (!gObj)
+      throw new Error('Please call init() with obj definitions before call check()!');
 
-    if (arr.length > 1) {
-      type = arr[1].trim();
-      if (type[0] == '[') {
-        val.should.be.instanceOf(Array);
+    if (!gObj.hasOwnProperty(name))
+      throw new Error(name + ' is not defined!');
+
+    gObj[name].forEach(function(prop){
+      var arr = prop.split(':')
+        , key = arr[0].trim()
+        , val
+        , type;
+
+      if (key.length == 0) {
+        obj.should.be.instanceOf(Array);
+
+        type = arr[1].trim();
         type = type.replace('[', '').replace(']', '').trim();
-
-        val.forEach(function(o){
+        obj.forEach(function(o){
           check(type, o);
         });
-      } else {
-        check(type, val);
+        return;
       }
 
-    }
-  });
-};
+      obj.should.have.property(key);
+      val = obj[key];
 
-exports.checkJSON = function(name, json_str) {
-  var obj = JSON.parse(json_str);
-  check(name, obj);
-  return obj;
-};
+      if (arr.length > 1) {
+        type = arr[1].trim();
+        if (type[0] == '[') {
+          val.should.be.instanceOf(Array);
+          type = type.replace('[', '').replace(']', '').trim();
+
+          val.forEach(function(o){
+            check(type, o);
+          });
+        } else {
+          check(type, val);
+        }
+
+      }
+    });
+  }
+
+  function checkJSON(name, json_str) {
+    var obj = JSON.parse(json_str);
+    check(name, obj);
+    return obj;
+  }
+
+  return {
+    check: check,
+    checkJSON: checkJSON
+  };
+}
+
+exports.init = init;
+
